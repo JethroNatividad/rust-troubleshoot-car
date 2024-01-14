f95255a71f73ea72ae71dc04fa2cea482f5fb019
JethroNatividad <natividad.jet@gmail.com>
2024-01-14 22:53:24 +0800

    expand problem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 // Write a program to troubleshoot a car, follow decision tree.
+// Inputs: Inputs to the decision tree, y or n
+// Process: process the decision tree
+// Outputs: the final output of the decision tree
 
 fn main() {
     println!("Hello, world!");
