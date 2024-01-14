90fea92fe365c16fcb97bef2a1f13f868ed86637
JethroNatividad <natividad.jet@gmail.com>
2024-01-14 23:06:38 +0800

    update problem

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Write a program to troubleshoot a car, follow decision tree.
 // Inputs: Inputs to the decision tree, y or n
 // Process: process the decision tree
-// Outputs: the final output of the decision tree
+// Outputs: the output of the decision tree
 
 fn main() {
     // Ask "Is the car silent when you turn the key?" (Yes - "Are the battery terminals corroded?" (Yes - "Clean terminals and try starting again.") | (No - "Replace cables and try again.") ) | (No - "Does the car make a clicking noise?" (Yes - "Replace the battery.") | (No - "Does the car crank up but fail to start?" (Yes - "Check spark plug connections.") | (No - "Does the engine start and then die?" (Yes - "Does your car have fuel injection?" (Yes - "Get it in for service.") | (No - "Check to ensure the choke is opening and closing.")))))
