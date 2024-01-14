ccd297a01e82da65696e20507048e78c812be2bd
JethroNatividad <natividad.jet@gmail.com>
2024-01-14 23:03:28 +0800

    write the system

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@
 // Outputs: the final output of the decision tree
 
 fn main() {
-    println!("Hello, world!");
+    // Ask "Is the car silent when you turn the key?" (Yes - "Are the battery terminals corroded?" (Yes - "Clean terminals and try starting again.") | (No - "Replace cables and try again.") ) | (No - "Does the car make a clicking noise?" (Yes - "Replace the battery.") | (No - "Does the car crank up but fail to start?" (Yes - "Check spark plug connections.") | (No - "Does the engine start and then die?" (Yes - "Does your car have fuel injection?" (Yes - "Get it in for service.") | (No - "Check to ensure the choke is opening and closing.")))))
 }
