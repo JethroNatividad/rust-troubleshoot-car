03a7c2ba959ea67a6d239aa88b01a7c3813164eb
JethroNatividad <natividad.jet@gmail.com>
2024-01-14 23:25:17 +0800

    format decision tree messages

--- src/main.rs
+++ src/main.rs
@@ -27,17 +27,17 @@ fn get_decision(prompt: &str) -> bool {
 fn main() {
     // Ask "Is the car silent when you turn the key?" (Yes - "Are the battery terminals corroded?" (Yes - "Clean terminals and try starting again.") | (No - "Replace cables and try again.") ) | (No - "Does the car make a clicking noise?" (Yes - "Replace the battery.") | (No - "Does the car crank up but fail to start?" (Yes - "Check spark plug connections.") | (No - "Does the engine start and then die?" (Yes - "Does your car have fuel injection?" (Yes - "Get it in for service.") | (No - "Check to ensure the choke is opening and closing.")))))
 
-    match get_decision("Is the car silent when you turn the key?") {
-        true => match get_decision("Are the battery terminals corroded?") {
+    match get_decision("Is the car silent when you turn the key? (y/n): ") {
+        true => match get_decision("Are the battery terminals corroded? (y/n): ") {
             true => println!("Clean terminals and try starting again."),
             false => println!("Replace cables and try again."),
         },
-        false => match get_decision("Does the car make a clicking noise?") {
+        false => match get_decision("Does the car make a clicking noise? (y/n): ") {
             true => println!("Replace the battery."),
-            false => match get_decision("Does the car crank up but fail to start?") {
+            false => match get_decision("Does the car crank up but fail to start? (y/n): ") {
                 true => println!("Check spark plug connections."),
-                false => match get_decision("Does the engine start and then die?") {
-                    true => match get_decision("Does your car have fuel injection?") {
+                false => match get_decision("Does the engine start and then die? (y/n): ") {
+                    true => match get_decision("Does your car have fuel injection? (y/n): ") {
                         true => println!("Get it in for service."),
                         false => println!("Check to ensure the choke is opening and closing."),
                     },
